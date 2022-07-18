b8004c233341f57addc46fd5f9fefdbd0f9fcb16
ihsancetinn <ihsancetin92@gmail.com>
2022-07-18 14:15:33 +0300

    function -- class

--- quiz.js
+++ quiz.js
@@ -1,9 +1,11 @@
-function Quiz(sorular) {
-    this.sorular = sorular;
-    this.soruIndex = 0;
-    this.dogruCevapSayisi = 0;
+class Quiz {
+    constructor(sorular) {
+        this.sorular = sorular;
+        this.soruIndex = 0;
+        this.dogruCevapSayisi = 0;
+    }
+    soruGetir() {
+        return this.sorular[this.soruIndex];
+    }
 }
 
-Quiz.prototype.soruGetir = function() {
-    return this.sorular[this.soruIndex];
-}
